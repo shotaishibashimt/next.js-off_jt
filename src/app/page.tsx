90fcabe359cb0c10cd4a3a679214754de37ab2cd
MT 石橋 翔太 <shota.ishibashi@metateam.co.jp>
2025-05-20 10:33:17 +0900

    APP-Router演習

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-const name: string = "Yuki";
+import type { Metadata } from "next";
+import Link from "next/link";
 
-export default function Home() {
+export default function TopPage() {
 	return (
 		<main>
-			<h1>こんにちは、{name}さん</h1>
+			<h1>ようこそ</h1>
+			<Link href="/memos">一覧ページへ</Link>
 		</main>
 	);
 }
