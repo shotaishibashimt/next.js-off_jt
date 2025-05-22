e80f5da558d414d71ab4505b74ca762b0dce43ca
MT 石橋 翔太 <shota.ishibashi@metateam.co.jp>
2025-05-22 15:22:17 +0900

    修正：空タグへ変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Page() {
 	const today = new Date().toLocaleDateString();
 
 	return (
-		<div>
+		<>
 			<h1>こんにちは！</h1>
 			<p>今日は{today}です。</p>
 			<LikeButton />
-		</div>
+		</>
 	);
 }
